514a5f92768acc63e084e61c5da441322ac98876
youvens <jocelynyouvens@gmail.com>
2025-05-25 19:38:14 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+function goHome() {
+  window.location.href = 'index.html';
+}
+
+
+
+const toggleBtn = document.getElementById('darkModeToggle');
+const body = document.body;
+
+// Charger la préférence enregistrée (localStorage)
+if (localStorage.getItem('darkMode') === 'enabled') {
+  body.classList.add('dark-mode');
+  toggleBtn.textContent = '☀️'; // changer icône lune → soleil
+}
+
+toggleBtn.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+  const isDark = body.classList.contains('dark-mode');
+  toggleBtn.textContent = isDark ? '☀️' : '🌙';
+
+  // Sauvegarder la préférence
+  if (isDark) {
+    localStorage.setItem('darkMode', 'enabled');
+  } else {
+    localStorage.setItem('darkMode', 'disabled');
+  }
+});
