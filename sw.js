7771388503e70a142793a6bd26d9e2213cd5d3c7
youvens <jocelynyouvens@gmail.com>
2025-10-10 17:51:42 -0700

    yy

--- sw.js
+++ sw.js
@@ -1,42 +1,40 @@
 const CACHE_NAME = 'seduction-cache-v1';
 const urlsToCache = [
-  './index.html',
-  './style.css',
-  './app.js',
-  './pages/aborder.html',
-  './pages/conversation.html',
-  './pages/confiance.html',
-  './pages/look.html',
-  './pages/sortir.html',
-  './pages/erreurs.html',
-  './pages/langage.html',
-  './pages/psychologie.html',
-  './pages/test.html',
-  './pages/faq.html',
-  './images/rencontre.svg',
-  './images/conversation.svg',
-  './images/confiance.svg',
-  './images/look.svg',
-  './images/sortir.svg',
-  './images/erreurs.svg',
-  './images/langage.svg',
-  './images/psychologie.svg',
-  './images/test.svg',
-  './images/faq.svg'
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/pages/aborder.html',
+  '/pages/conversation.html',
+  '/pages/confiance.html',
+  '/pages/look.html',
+  '/pages/sortir.html',
+  '/pages/erreurs.html',
+  '/pages/langage-corporel.html',
+  '/pages/psychologie.html',
+  '/pages/test.html',
+  '/pages/faq.html',
+  '/images/rencontre.svg',
+  '/images/conversation.svg',
+  '/images/confiance.svg',
+  '/images/look.svg',
+  '/images/sortir.svg',
+  '/images/erreurs.svg',
+  '/images/langage.svg',
+  '/images/psychologie.svg',
+  '/images/test.svg',
+  '/images/faq.svg',
+  '/images/icon-192.png',
+  '/images/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Caching app assets...');
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
